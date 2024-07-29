eba73b0b88f73df57ac37a2c8754d49be839331b
ripat123 <ripatrabbi2@outlook.com>
2024-07-29 17:09:33 +0600

    29-07-2024

--- app/src/main/java/com/sbitbd/farmerassist/ui/home/HomeViewModel.java
+++ app/src/main/java/com/sbitbd/farmerassist/ui/home/HomeViewModel.java
@@ -4,16 +4,18 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.sbitbd.farmerassist.DataModel.WeatherModel;
+import com.sbitbd.farmerassist.Repository.WeatherRepository;
+
+import javax.inject.Inject;
+
 public class HomeViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
-
-    public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+    private final WeatherRepository weatherRepository;
+    private MutableLiveData<WeatherModel> data = new MutableLiveData<>();
+    @Inject
+    public HomeViewModel(WeatherRepository weatherRepository) {
+        this.weatherRepository = weatherRepository;
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
 }
