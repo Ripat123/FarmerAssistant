d310e6500acf7f9cce5c9a130ae739106474eb96
ripat123 <ripatrabbi2@outlook.com>
2024-08-10 18:42:29 +0600

    10-08-2024

--- app/src/main/java/com/sbitbd/farmerassist/ui/question/DashboardFragment.java
+++ app/src/main/java/com/sbitbd/farmerassist/ui/question/DashboardFragment.java
@@ -7,11 +7,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.sbitbd.farmerassist.databinding.FragmentDashboardBinding;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
@@ -24,6 +28,16 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
 
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView();
+    }
+
+    private void initView(){
+
     }
 
     @Override
--- app/src/main/java/com/sbitbd/farmerassist/ui/question/DashboardViewModel.java
+++ app/src/main/java/com/sbitbd/farmerassist/ui/question/DashboardViewModel.java
@@ -4,16 +4,45 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.ai.client.generativeai.GenerativeModel;
+import com.google.ai.client.generativeai.type.Content;
+import com.google.ai.client.generativeai.type.GenerateContentResponse;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+
+@HiltViewModel
 public class DashboardViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    @Inject
+    private GenerativeModel generativeModel;
 
-    public DashboardViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
-    }
+    public void generateText(String prompt) {
+        try {
+            Content content = new Content.Builder()
+                    .addText(prompt)
+                    .build();
+            ListenableFuture<GenerateContentResponse> response = generativeModel.generateContent(content);
 
-    public LiveData<String> getText() {
-        return mText;
+            Futures.addCallback(response, new FutureCallback<GenerateContentResponse>() {
+                @Override
+                public void onSuccess(GenerateContentResponse result) {
+
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+
+                }
+            },command -> {
+
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
